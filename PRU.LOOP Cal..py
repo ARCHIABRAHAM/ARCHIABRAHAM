10c9f7883cdc567f70f9554d07786059d4fc0a79
ARCHIABRAHAM <109981372+ARCHIABRAHAM@users.noreply.github.com>
2022-08-17 13:04:02 -0300

    Add files via upload

---
+++ PRU.LOOP Cal..py
@@ -0,0 +1,13 @@
+#CALCULADORA
+#YOU CAN MODIFY THE LOOP
+#PUEDE SERVIR ESTE CÓDIGO, AUNQ MUY SENCILLO ME AYUDO MUCHO CUANDO ESTABA APRENDIENDO
+#THIS CODE HELPED ME A LOT FOR VARIOUS OPERATIONS, IT IS VERY SIMPLE
+
+
+
+num = 1
+digitoAcalcular = int(input("Ingrese el numero a calcular:"))
+for i in range(10):
+    print(f"{digitoAcalcular} x {num} = {digitoAcalcular * num}")
+    num = num + 1
+
